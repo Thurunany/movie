da4835a029a702cb96efde1928489563df5ab005
Thurunany <thutymsoares@gmail.com>
2022-07-08 02:47:54 -0300

    Adding footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+<v-footer class="container">
+    <v-card class="float-end mb-1">
+        <v-card-text>
+            <p class="mb-1">Movie App is a web project, using a movie database API!</p>
+            <p class="mb-0">So if You liked what You saw please <a href="https://github.com/Thurunany/movie-shop">Visit the Github repositorie.</a></p>
+        </v-card-text>
+    </v-card>
+</v-footer>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
