3c1946ce4e9f50881436748771ebbfe2403276ac
Thurunany <thutymsoares@gmail.com>
2022-07-08 04:10:01 -0300

    Changing Footer and NavBar layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-<v-footer >
+<v-footer class="footer footer-dark bg-dark shadow-sm text-white">
     <v-card-text>
         <p class="float-end mb-1">
-        <a href="#" class="btn btn-outline-primary" role="button">Back to the top</a>        
+        <a href="#" class="btn btn-dark text-white" role="button">Back to the top</a>        
         </p>
         <p class="mb-1">Movie App is a web project made with The Movie Database API!</p>
         <p class="mb-0">So if You liked please check out the <a href="https://github.com/Thurunany/movie-shop">Github repositorie.</a></p>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,19 @@
 <template>
+
+<div id="app">
 <nav class="navbar navbar-dark bg-dark shadow-sm">
-    <v-app-bar>
-        <a class="navbar-brand">:)</a>
-        <v-form class="d-flex" role="search" >
-        <v-autocomplete class="form-control me-2" type="search" placeholder="Search" aria-label="Search" :itens="movies" item-text="title" item-value="id" id="search">
-            <template v-slot:item="{item}">
+  <div class="container">
+    <a class="navbar-brand text-light fs-1 fw-bold">Movie App</a>
+    <v-form class="d-flex" role="search" >
+      <v-autocomplete class="form-control me-2" type="search" placeholder="Search" aria-label="Search" :itens="movies" item-text="title" item-value="id" id="search">
+        <template v-slot:item="{item}">
               <v-btn text :to="`/movie/${item.id}`">{{item.title}}</v-btn>
-            </template>
-            </v-autocomplete>
-        </v-form> 
-    </v-app-bar>
+        </template>
+      </v-autocomplete>
+    </v-form>
+  </div>
 </nav>
+</div>
 </template>
 
 <script>
