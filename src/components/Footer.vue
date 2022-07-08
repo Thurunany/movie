4de60038fb726d6052740a04f55796375c26a8ab
Thurunany <thutymsoares@gmail.com>
2022-07-08 03:49:57 -0300

    Changing the footer layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,12 @@
 <template>
-<v-footer class="container">
-    <v-card class="float-end mb-1">
-        <v-card-text>
-            <p class="mb-1">Movie App is a web project, using a movie database API!</p>
-            <p class="mb-0">So if You liked what You saw please <a href="https://github.com/Thurunany/movie-shop">Visit the Github repositorie.</a></p>
-        </v-card-text>
-    </v-card>
+<v-footer >
+    <v-card-text>
+        <p class="float-end mb-1">
+        <a href="#" class="btn btn-outline-primary" role="button">Back to the top</a>        
+        </p>
+        <p class="mb-1">Movie App is a web project made with The Movie Database API!</p>
+        <p class="mb-0">So if You liked please check out the <a href="https://github.com/Thurunany/movie-shop">Github repositorie.</a></p>
+    </v-card-text>
 </v-footer>
 </template>
 
