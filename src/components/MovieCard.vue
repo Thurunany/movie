222b7412f18e0df25be18a6e1f3e1d915fc26e6d
Thurunany <thutymsoares@gmail.com>
2022-07-08 03:58:18 -0300

    changing Movie cards layout

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,19 +1,15 @@
 <template>
 <v-hover v-slot="{hover}" open-delay="200" class="col">
     <v-card :elevation="hover ? 16:2" :class="{'on-hover' : hover}" class="card-body" height="100%">
-        <router-link :to="`/movie/${movie.id}`">
-            <v-img :src="posterPath" class="bd-placeholder-img card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></v-img>
-        </router-link>
+      <v-img :src="posterPath" class="bd-placeholder-img card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></v-img>
         <v-card-title class="subtitle-2">{{movie.title}}</v-card-title>
         <v-card-text class="card-text align-items-center">
             <span v-for="(genre, index) in movie.genre_ids" :key="genre">
                   {{genreTypeName(genre, index)}}
             </span>
-            <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
+            <div class="d-flex justify-content-between align-items-center"> 
                 <!-- Checar se o botão funciona mais tarde! -->
-                  <v-btn type="button" class="btn btn-sm btn-outline-secondary" :to="`/movie/${movie.id}`">Info</v-btn>
-                </div>
+                <v-btn depressed :to="`/movie/${movie.id}`">Info</v-btn>
                 <small class="text-muted my-4 subtitle-2 ">
                   <v-rating :value="movie.vote_average / 2" color="amber" dense half-increments readonly size="14"> </v-rating>
                 <div class="grey--text ml-4">
