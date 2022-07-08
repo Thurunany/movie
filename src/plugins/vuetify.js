d2d5bc59f871652d70bae47d91e3094722044b75
Thurunany <thutymsoares@gmail.com>
2022-07-08 02:30:46 -0300

    adding api to vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import api from "../services/api"
 
 Vue.use(Vuetify);
+Vue.prototype.$http = api;
 
 export default new Vuetify({
 });
