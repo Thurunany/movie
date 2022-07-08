b6c311ae10bd7a19322b2788422dc787a648fbfb
Thurunany <thutymsoares@gmail.com>
2022-07-08 03:33:32 -0300

    Changing HomeView for the app template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PopularMovies />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PopularMovies from '@/components/PopularMovies.vue'
 
 export default {
-  name: 'HomeView',
+  name: 'Home_app',
   components: {
-    HelloWorld
-  }
+    PopularMovies,
+}
 }
 </script>
