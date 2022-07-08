58790949ecd3e27a2691525ca59dc780bb7f1244
Thurunany <thutymsoares@gmail.com>
2022-07-08 03:07:18 -0300

    Adding the movie carts

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,62 @@
+<template>
+<v-hover v-slot="{hover}" open-delay="200" class="col">
+    <v-card :elevation="hover ? 16:2" :class="{'on-hover' : hover}" class="card-body" height="100%">
+        <router-link :to="`/movie/${movie.id}`">
+            <v-img :src="posterPath" class="bd-placeholder-img card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></v-img>
+        </router-link>
+        <v-card-title class="subtitle-2">{{movie.title}}</v-card-title>
+        <v-card-text class="card-text align-items-center">
+            <span v-for="(genre, index) in movie.genre_ids" :key="genre">
+                  {{genreTypeName(genre, index)}}
+            </span>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <!-- Checar se o botão funciona mais tarde! -->
+                  <v-btn type="button" class="btn btn-sm btn-outline-secondary" :to="`/movie/${movie.id}`">Info</v-btn>
+                </div>
+                <small class="text-muted my-4 subtitle-2 ">
+                  <v-rating :value="movie.vote_average / 2" color="amber" dense half-increments readonly size="14"> </v-rating>
+                <div class="grey--text ml-4">
+                  {{movie.vote_average * 10}} % | {{movie.release_date}}
+                </div>
+                </small>
+            </div>
+        </v-card-text>
+    </v-card>
+</v-hover>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: {
+      required: true,
+    },
+     genres: {
+      required: true,
+    }
+  },
+  computed: {
+    posterPath(){
+      return "https://image.tmdb.org/t/p/w500/" + this.movie.poster_path
+    }
+  },
+  methods: {
+    genreTypeName(genraId, index) {
+
+      for(const item of this.genres) {
+        if(item.id == genraId) {
+          if(this.movie.genre_ids.length - 1 == index){
+            return item.name ;
+          }else {
+            return item.name + ",";
+          }
+          
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
