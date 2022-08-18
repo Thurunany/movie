78a4bb8a0e54a3cc5d0bdabec8ffcb3d974e9ad5
Thuany <50593898+Thurunany@users.noreply.github.com>
2022-08-17 23:43:53 -0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
         <a href="#" class="btn btn-dark text-white" role="button">Back to the top</a>        
         </p>
         <p class="mb-1">Movie App is a web project made with The Movie Database API!</p>
-        <p class="mb-0">So if You liked please check out the <a href="https://github.com/Thurunany/movie-shop">Github repositorie.</a></p>
+        <p class="mb-0">So if You liked please check out the <a href="https://github.com/Thurunany/movie">Github repositorie.</a></p>
     </v-card-text>
 </v-footer>
 </template>
